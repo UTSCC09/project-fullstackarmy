e5b06bd04e929a5405b8a66549b2bada6c545012
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-07 22:29:25 -0500

    conflict resolve

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,58 @@
 const express = require('express');
-const path = require('path');
-const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const fs = require('fs');
 const multer  = require('multer');
-const graphql = require('graphql');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const mongoose = require('mongoose');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+// For uploads 
+// const fs = require('fs');
+// const multer  = require('multer');
+
 
 const app = express();
-let upload = multer({ dest: path.join(__dirname, 'uploads')});
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// For uploads 
+// let upload = multer({ dest: path.join(__dirname, 'uploads')});
+// app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
-app.use('/graphql', graphqlHTTP({
-    schema, 
-    pretty: true,
-    graphiql: true,
+
+
+
+app.use('/api', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            vaccinationData: [String!]!
+        }
+        
+        type RootMutation {
+            createVaccinationData(date: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `), 
+    rootValue: {
+        vaccinationData: () => {
+            return ['Poop', 'pooP', 'PoOp'];
+        },
+        createVaccinationData: (args) => {
+            const vaccinationDataDate = args.date;
+            return vaccinationDataDate;
+        }
+    },
+    graphiql: true
 }))
+
 app.use(session({
     secret: 'this is a secure secret amirite?',
     resave: false,
