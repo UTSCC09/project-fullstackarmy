4c3bcfbd2e2be71aa0fa17748931362816f8297f
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-11 21:47:21 -0500

    Create rootAPI.js

---
+++ frontend/src/api/rootAPI.js
@@ -0,0 +1,5 @@
+export default class rootAPI {
+  // Code was inspired from https://medium.com/swlh/react-api-call-best-practice-c78e14a7e150
+  // todo this will be the root class that api's will extend that will have methods
+  // todo to send a request to the backend using apollo client
+}
