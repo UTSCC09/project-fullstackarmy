c3dc6e3099f6d7489406dcce4cee183a2ffb9932
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-19 18:11:42 -0400

    Create VaccMap.tsx

---
+++ frontendts/src/components/map/VaccMap.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MapContainer from './MapContainer';
+
+interface Props {
+}
+
+const VaccMap: React.FC<Props> = () => {
+  //? get the data here and pass it to the map container
+  const GET_MAP_VACC_DATA = 
+
+  return (
+    <MapContainer />
+  )
+}
+
+export default VaccMap
+
+
