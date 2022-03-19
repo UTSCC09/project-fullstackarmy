b9d7f14e54b769654c6500cfd6c41938ee4640cd
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-19 18:11:36 -0400

    Update StatusTab.tsx

--- frontendts/src/components/tabs/StatusTab.tsx
+++ frontendts/src/components/tabs/StatusTab.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import VaccMap from '../map/VaccMap'
 
 export const StatusTab = () => {
+  
+
   return (
     <div>
-        <h1>Vaccination Status</h1>
+      <h1>Vaccination Status</h1>
+      <VaccMap />
     </div>
   )
 }
