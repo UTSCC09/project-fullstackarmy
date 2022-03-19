91b2a7608f2a6b6affb6fd5959feaf52ea2639e8
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-19 18:12:17 -0400

    marker for google map experiment

---
+++ frontendts/src/components/map/Marker.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+    position: google.maps.LatLngLiteral;
+    map?: google.maps.Map;
+}
+
+const Marker: React.FC<Props> = ({position, map}) => {
+  const [marker, setMarker] = useState<google.maps.Marker | null>(null);
+
+  useEffect(() => {
+    setMarker(new google.maps.Marker({})) ;
+  }, []);
+
+  if (marker) {
+    marker.setMap(map);
+    marker.setPosition(position);
+  }
+
+  return null;
+}
+
+export default Marker
+
+// ? Most likely will have to do something similar with the features
+// ? that I will be adding
