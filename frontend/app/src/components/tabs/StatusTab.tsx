aa8c32aa9003e5deecfa151fc55e2b8ea9011743
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-20 18:49:44 -0400

    Update StatusTab.tsx

--- frontend/app/src/components/tabs/StatusTab.tsx
+++ frontend/app/src/components/tabs/StatusTab.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HerdImmunityBarChart from "../charts/HerdImmunityBarChart";
+import BoosterVaccMap from '../map/BoosterVaccMap';
+import FullVacMap from '../map/FullVaccMap';
+import VaccMap from '../map/VaccMap';
 
 export const StatusTab = () => {
   return (
     <div>
-        <HerdImmunityBarChart />
+      <HerdImmunityBarChart />
+      <VaccMap />  
+      <FullVacMap />  
+      <BoosterVaccMap />  
     </div>
   )
 }
