fd4cbd1f570ea50b2a38d780ff4076423132760c
Raha Gharadaghi <rahagh79@gmail.com>
2022-03-20 19:44:09 -0400

    created background information tab

--- frontend/app/src/components/Credits.tsx
+++ frontend/app/src/components/Credits.tsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 export const Credits = () => {
   return (
-    <div>
+    <div className='credits'>
         <h1>Credits</h1>
+        <h2>UI Components</h2>
+        <h4>Logo Font</h4>
+        <li>developed by <a href="https://fonts.google.com/specimen/Coda">Google Fonts</a></li>
+        <li>downloaded from <a href="https://www.dafontfree.io/coda-font/#google_vignette">Dafont Free</a></li>
+        <li>licensed under the <a href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL">Open Font License</a></li>
+        <h4>Background Information Tab: </h4>
+        <h5>Background GIF</h5>
+        <li>created by <a href="https://tenor.com/view/blue-white-gradient-carrd-gif-23556830">barash (Tenor GIF)</a></li>
+        <h5>World Icon PNG</h5>
+        <li>created by <a href="https://pixabay.com/vectors/globe-mask-world-covid-coronavirus-5636554/">rfind / 35 images (Pixabay)</a></li>
+        <h5>Destination Icon PNG</h5>
+        <li>downloaded from <a href="https://www.pngitem.com/middle/TRRwmhi_address-transparent-location-icon-yellow-hd-png-download/">PNGITEM</a></li>
+        <h5>Vaccine Icon PNG</h5>
+        <li>created by <a href="https://iconscout.com/icon/corona-vaccine-2485203">Rank Sol (iconscout)</a></li>
+        <h5>Blue Arrow PNG</h5>
+        <li>created by <a href="https://pixabay.com/images/id-310632/">Clker-Free-Vector-Images (Pixabay)</a></li>
     </div>
   )
 }
