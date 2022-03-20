62aca671b8341941e3122f54cd45482c210ce5b3
Raha <Owner@Owners-MacBook-Air-2.local>
2022-03-20 15:17:00 -0400

    Updated folder structure

--- frontend/src/components/TabHeader.js
+++
@@ -1,5 +0,0 @@
-export const TabHeader = ({title, imgPath}) => {
-  return (
-    <div className="tab-header"><div className="tab-icon" style={{backgroundImage: `url(${imgPath})`}}></div>{title}</div>
-  )
-}
