5c30cf1e0eb50a58d9142aea21806e8bece9d182
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-22 16:40:54 -0400

    styling fixes to styling tab

--- frontend/app/src/components/tabs/StatusTab.tsx
+++ frontend/app/src/components/tabs/StatusTab.tsx
@@ -3,14 +3,15 @@ import HerdImmunityBarChart from "../charts/HerdImmunityBarChart";
 import BoosterVaccMap from '../map/BoosterVaccMap';
 import FullVacMap from '../map/FullVaccMap';
 import VaccMap from '../map/VaccMap';
-import './StatusTab.css';
+import './styles/StatusTab.css';
 
 export const StatusTab = () => {
+
   return (
     <div>
       <HerdImmunityBarChart />
       <h2>First Vaccination Dose Heat Map</h2>
-      <VaccMap /> 
+      {/* <VaccMap />  */}
       <h2>Second Vaccination Dose Heat Map</h2>
       <FullVacMap />  
       <h2>Booster Dose Heat Map</h2>
