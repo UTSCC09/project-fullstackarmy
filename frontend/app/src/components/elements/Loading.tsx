148ab5fddb5f8992aa266d3c05c74ea6260a3256
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-22 16:39:27 -0400

    loading component

---
+++ frontend/app/src/components/elements/Loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Loading.css'
+
+interface Props {
+}
+
+const Loading: React.FC<Props> = () => {
+
+  return (
+    <div className="dot-load"></div>    
+  )
+}
+
+export default Loading
+
+
