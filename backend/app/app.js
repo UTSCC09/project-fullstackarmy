255eed5e67a62144f7efa2a04fa9ec53be6d1bcb
Aaren Chu <aarenchu@gmail.com>
2022-03-23 22:10:05 -0400

    trying to see if cors is causing the issue

--- backend/app/app.js
+++ backend/app/app.js
@@ -44,6 +44,7 @@ app.use(cors({
     if(allowedOrigins.indexOf(origin) === -1){
       let msg = 'The CORS policy for this site does not ' +
                 'allow access from the specified Origin.';
+      console.log(msg)
       return callback(new Error(msg), false);
     }
     return callback(null, true);
