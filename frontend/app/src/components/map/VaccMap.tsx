8a5a492269cacb08441c1a67d38bc44d1dfb634d
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-03-27 22:05:22 -0400

    Update VaccMap.tsx

--- frontend/app/src/components/map/VaccMap.tsx
+++ frontend/app/src/components/map/VaccMap.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
 import Loading from '../elements/Loading';
 import VaccinationMap from './VaccinationMap';
 
@@ -20,8 +20,8 @@ const GET_VACC_MAP_COUNTRY_DATA = gql`
 `;
 
 const GET_VACC_MAP_CONTINENT_DATA = gql`
-  query CountryVaccMapData($startDate: String!, $endDate: String!){
-    countryVaccMapData(startDate: $startDate, endDate: $endDate) {
+  query ContinentVaccMapData($startDate: String!, $endDate: String!){
+    continentVaccMapData(startDate: $startDate, endDate: $endDate) {
       isoCode
       peopleVaccinatedPerHundred
     }
@@ -31,12 +31,25 @@ const GET_VACC_MAP_CONTINENT_DATA = gql`
 const mapName = 'FirstDoseVaccMap';
 const featureValueName = 'peopleVaccinatedPerHundred';
 
-let featureData;
-
 const VaccMap: React.FC<Props> = () => {
  
-  const { loading, error, data } = useQuery(GET_VACC_MAP_COUNTRY_DATA,
+  const [featureData, setFeatureData] = useState(null);
+
+  // Get both data at once, even though some of it may not be used by the user
+  // this is to ensure the user experience is fast and high quality
+  const countryData = useQuery(GET_VACC_MAP_COUNTRY_DATA, 
     {
+      variables: {
+        startDate,
+        endDate,
+      },
+      onCompleted: (data) => {
+        setFeatureData(data.countryVaccMapData);
+      }
+    }
+  );
+
+  const continentData = useQuery(GET_VACC_MAP_CONTINENT_DATA, {
       variables: {
         startDate,
         endDate,
@@ -44,21 +57,33 @@ const VaccMap: React.FC<Props> = () => {
     }
   );
   
-  //todo should make error components
-  if (loading) return <Loading />;
-  if (error) return null;
+  /** 
+  * Makes the backend call to get the associated map data for country/continent
+  * @param {boolean} isContinentCall
+  */
+  const continentDataCall = (isContinentCall: boolean) => {
 
-  if (data) {
-    featureData = data.countryVaccMapData
+    if (isContinentCall) {
+      setFeatureData(continentData.data.continentVaccMapData);
+    } else {
+      setFeatureData(countryData.data.countryVaccMapData);
+    }
   }
+
+  // add error component
+  if (countryData && countryData.loading) return <Loading />;
+  if (continentData && continentData.loading) return <Loading />;
+  if (countryData && countryData.error) return null;
+  if (continentData && continentData.error) return null;
   
   return (
     <VaccinationMap 
       mapName={mapName} 
-      binaryFeatureStyling={false} 
-      continentToggle={true} 
+      binaryFeatureStyling={true}
+      continentToggle={true}
       featureData={featureData} 
       featureValueName={featureValueName}
+      continentDataCall={continentDataCall}
     />
   )
 }
