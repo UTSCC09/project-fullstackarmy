0dc9648de87113ca9b877917fa6a3ed1a069da24
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-04-05 15:48:52 -0400

    err msg

---
+++ backend/app/graphql/resolvers/constants.js
@@ -0,0 +1,3 @@
+const ErrorMessage = `An unknown errror occured from our side! Please rest assured that our team is working hard to ensure this is resolved at the fastest possible time.`;
+
+exports.ErrorMessage = ErrorMessage;
