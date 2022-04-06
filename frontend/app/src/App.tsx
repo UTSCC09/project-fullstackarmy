8d9d08c7333a5e9c074258009f6a5b4d76d3e9a7
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-04-05 23:49:01 -0400

    hot fix
    removing css file import + correcting component import

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
-import { TabNav } from './components/TabNav';
+import TabNav from './components/TabNav';
 import { Footer } from './components/Footer';
 import { DataSources } from './components/DataSources';
 import { Credits } from './components/Credits';
