4545f6edcd86a2dd3e00f0b0582a357ae0b0172f
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-04-08 02:25:56 -0400

    changed the dark mode toggle

--- frontend/app/src/components/ConfigBar.tsx
+++ frontend/app/src/components/ConfigBar.tsx
@@ -7,7 +7,6 @@ import Typography from '@mui/material/Typography';
 import { CountriesFilter } from './CountriesFilter';
 import { DateFilter } from './DateFilter';
 import { useTranslation } from 'react-i18next';
-import { ColorModeToggle } from './ColorModeToggle';
 import SavedConfigs from './SavedConfigs';
 
 interface Props {
@@ -49,14 +48,6 @@ const ConfigBar: React.FC<Props> = ({ open, handleClose }) => {
         {t('configbar.date')}
       </Typography>
       <DateFilter />
-
-      <Divider sx={{ marginTop: '14px' }} />
-
-      <Typography variant='subtitle1' align='left' sx={{ marginLeft: '14px' }}>
-        {t('configbar.mode')}
-      </Typography>
-
-      <ColorModeToggle />
 
       <Divider sx={{ marginTop: '14px' }} />
 
