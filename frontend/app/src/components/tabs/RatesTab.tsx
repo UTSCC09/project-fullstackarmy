d232ff86fa8edc78981faea94178f80c3078ca6a
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-04-08 21:56:33 -0400

    last min removal

--- frontend/app/src/components/tabs/RatesTab.tsx
+++ frontend/app/src/components/tabs/RatesTab.tsx
@@ -2,31 +2,21 @@ import { t } from 'i18next';
 import HerdImmunityTimeSeriesChart from '../charts/HerdImmunityTimeSeriesChart';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import {RatesEstimatorDropdown} from './components/RatesEstimatorDropdown';
 
 interface Props {}
 
 const RatesTab: React.FC<Props> = () => {
   return (
     <div className='tab'>
-      <Paper elevation={2} sx={{margin: 2, padding: 2}}>
-        <Typography variant='body1'>
-          {t('ratesTab.p1')}
-        </Typography>
+      <Paper elevation={2} sx={{ margin: 2, padding: 2 }}>
+        <Typography variant='body1'>{t('ratesTab.p1')}</Typography>
       </Paper>
-      <Paper elevation={2} sx={{margin: 2, padding: 2}}>
+      <Paper elevation={2} sx={{ margin: 2, padding: 2 }}>
         <HerdImmunityTimeSeriesChart />
       </Paper>
-      <Paper elevation={2} sx={{margin: 2, padding: 2}}>
-        <Typography variant='body1'>
-          {t('ratesTab.p2')}
-        </Typography> 
-        <Typography variant='body1'>
-        {t('ratesTab.p3')}
-        </Typography>
-      </Paper>
-      <Paper elevation={2} sx={{margin: 2, padding: 2}}>
-        <RatesEstimatorDropdown/>
+      <Paper elevation={2} sx={{ margin: 2, padding: 2 }}>
+        <Typography variant='body1'>{t('ratesTab.p2')}</Typography>
+        <Typography variant='body1'>{t('ratesTab.p3')}</Typography>
       </Paper>
     </div>
   );
